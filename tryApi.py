11f988dae125e4c84aecb4ad3c65a65e5e79a703
silvio <silvio@debian>
2014-06-23 19:21:19 +0200

    yo

---
+++ tryApi.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env 
+# -*- coding: utf-8 -*-
+
+# MODULES
+
+import web
+from getImgUrl import search
+from time import sleep
+
+
+
+while True:
+	term = search()
+	print term[0]
+	print term[1]
+	sleep(1)
