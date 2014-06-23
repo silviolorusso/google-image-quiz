a68ea2f31a21997184c2d438aaa35a6151a71fcc
silvio <silvio@debian>
2014-06-23 19:13:37 +0200

    app

---
+++ app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env 
+# -*- coding: utf-8 -*-
+
+# MODULES
+
+import web
+from getImgUrl import search
+
+render = web.template.render('templates/')
+
+urls = (
+  '/', 'index'
+)
+
+class index:
+	def GET(self):
+		term = search()
+		return render.index(term[0], term[1])
+
+if __name__ == "__main__": 
+	app = web.application(urls, globals())
+	app.run()  
