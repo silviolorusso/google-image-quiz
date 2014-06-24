204bf44d64c165c7cfa2b8fb27701ce258cec52a
lorussian <silviolorusso@gmail.com>
2014-06-24 12:37:58 +0200

    style

--- static/quiz.js
+++ static/quiz.js
@@ -48,7 +48,7 @@ $('#try').click( function(){
 	if (getUsrQuery() == query) {
 		alert('Correct! You guessed the query in ' + attempts + ' attempts using ' + hints + " hints.");
 	} else {
-		$('#background, .alert').show(function(){
+		$('#background, .alert').show(0, function(){
 			setTimeout(function(){
 				$('#background, .alert').hide();
 			}, 1000);
