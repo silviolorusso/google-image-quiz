3703d11d11506b3e5bb7b39611cd6e8ca203cfd7
silvio <silvio@debian>
2014-06-25 00:19:03 +0200

    generalize

--- mkDb.py
+++ mkDb.py
@@ -8,11 +8,12 @@
 import sqlite3
 import urllib2
 import simplejson
+from getImgUrl import search
 from time import sleep
 
 # FUNCTIONS
 
-def getWord():
+def getWords():
 	words = []
 	f = open("list.txt", "r")
 	for line in f:
@@ -21,34 +22,9 @@ def getWord():
 		words.append(line)
 	return words
 
-def search(term):
-	fetcher = urllib2.build_opener()
-	searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + term
-	f = fetcher.open(searchUrl)
-	imgs = simplejson.load(f)
-	# check if a result exists
-	try:
-		imgUrl = imgs['responseData']['results'][0]['unescapedUrl']
-		# check if it's a broken link
-		class HeadRequest(urllib2.Request):
-			def get_method(self):
-				return "HEAD"
-		try:
-			response = urllib2.urlopen(HeadRequest(imgUrl))
-		except:
-			print('Error: forbidden!')
-			search()
-		# reverse the query
-		rterm = term[::-1]
-		print(rterm + ' ' + imgUrl)
-		return([rterm, imgUrl])
-	except IndexError:
-		print('Error: no images found!')
-		return
-
 # WORK
 
-words = getWord()
+words = getWords()
 
 # create / connect to db
 conn = sqlite3.connect(r"./queries.db")
@@ -77,39 +53,3 @@ with conn:
     		sleep(10)
     		pass
     	sleep(3)
-
-# with conn:
-#     cur = conn.cursor()
-#     # drop Queries
-#     try:
-#     	cur.execute('''DROP TABLE Queries''')
-#     except:
-#     	pass
-#     # create Queries
-#     try:
-#     	cur.execute("CREATE TABLE Queries(Id INT, Query TEXT, Url TEXT)")
-#     except:
-#     	pass
-#     i = 0
-#     for word in words:
-#     	try:
-#     		output = search(word)
-#     		print output[0]
-#     		print output[1]
-#     		imgUrl = output[1]
-#     		# check if it's a broken link
-#     		class HeadRequest(urllib2.Request):
-#     			def get_method(self):
-#     				return "HEAD"
-# 				try:
-# 					response = urllib2.urlopen(HeadRequest(imgUrl))
-# 				except:
-# 					pass
-# 				cur.execute("INSERT INTO Queries VALUES(?,?,?)", (i, output[0], output[1]))
-# 				conn.commit()
-# 				i += 1
-#     	except:
-#     		print('Error')
-#     		sleep(10)
-#     		pass
-#     	sleep(3)
