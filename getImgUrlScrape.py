4fe2c1d284111cf6f0b9753dd708a887c48dbe2f
lorussian <silviolorusso@gmail.com>
2014-06-25 12:29:21 +0200

    db

--- getImgUrlScrape.py
+++ getImgUrlScrape.py
@@ -27,7 +27,7 @@ def searchScrape(term):
 	searchUrl = "http://google.com/images?source=hp&q=" + term
 	req = urllib2.Request(searchUrl, None, headers)
 	html = urllib2.urlopen(req).read()
-	soup = BeautifulSoup(html, "lxml")
+	soup = BeautifulSoup(html)
 	links = soup.findAll("a", { "class" : "rg_l" })
 	messyLink = links[0].get("href")
 	imgUrl = re.search( r'imgurl=(.*)&imgrefurl', messyLink).groups()[0]
