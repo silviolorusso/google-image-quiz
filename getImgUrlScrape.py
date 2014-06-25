29328853cf8e796f502f415e8746aeea1611218c
lorussian <silviolorusso@gmail.com>
2014-06-25 12:21:17 +0200

    db

--- getImgUrlScrape.py
+++ getImgUrlScrape.py
@@ -23,7 +23,7 @@ def randWord():
 	return(rand)
 
 def searchScrape(term):
-	headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36' }
+	headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0' }
 	searchUrl = "http://google.com/images?source=hp&q=" + term
 	req = urllib2.Request(searchUrl, None, headers)
 	html = urllib2.urlopen(req).read()
