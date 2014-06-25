36ea47def496677bf34de909439b29d1c25fa4a9
lorussian <silviolorusso@gmail.com>
2014-06-25 10:56:05 +0200

    scraper

---
+++ getImgUrlScrape.py
@@ -0,0 +1,51 @@
+#!/home2/silviolo/python/bin/python 
+# -*- coding: utf-8 -*-
+
+# Get first image url from Google query
+
+# MODULES
+
+import urllib2
+import random
+from bs4 import BeautifulSoup
+import re
+
+# FUNCTIONS
+
+def randWord():
+	words = []
+	f = open("list.txt", "r")
+	for line in f:
+		# remove /n 
+		line = line.rstrip()
+		words.append(line)
+	rand = random.choice(words)
+	return(rand)
+
+def searchScrape(term):
+	headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36' }
+	searchUrl = "http://google.com/images?source=hp&q=" + term
+	req = urllib2.Request(searchUrl, None, headers)
+	html = urllib2.urlopen(req).read()
+	soup = BeautifulSoup(html)
+	links = soup.findAll("a", { "class" : "rg_l" })
+	messyLink = links[0].get("href")
+	imgUrl = re.search( r'imgurl=(.*)&imgrefurl', messyLink).groups()[0]
+	# check if it's a broken link
+	class HeadRequest(urllib2.Request):
+		def get_method(self):
+			return "HEAD"
+	try:
+		response = urllib2.urlopen(HeadRequest(imgUrl))
+	except:
+		print('Error: forbidden!')
+		searchScrape()
+	# reverse the query
+	rterm = term[::-1]
+	print(rterm + ' ' + imgUrl)
+	return([rterm, imgUrl])
+
+# TEST
+
+if __name__ == "__main__":
+	searchScrape(randWord())
---
+++ mkDbScrape.py
@@ -0,0 +1,57 @@
+#!/home2/silviolo/python/bin/python 
+# -*- coding: utf-8 -*-
+
+# Create Db with query, image url
+
+# MODULES
+
+import sqlite3
+import urllib2
+from getImgUrlScrape import searchScrape
+from time import sleep
+import os
+
+# FUNCTIONS
+
+def getWords():
+	words = []
+	f = open("list.txt", "r")
+	for line in f:
+		# remove /n 
+		line = line.rstrip()
+		words.append(line)
+	return words
+
+# WORK
+
+words = getWords()
+
+# create / connect to db
+conn = sqlite3.connect(r"./queries-temp.db")
+
+with conn:
+    cur = conn.cursor()
+    # drop Queries
+    try:
+    	cur.execute('''DROP TABLE Queries''')
+    except:
+    	pass
+    # create Queries
+    try:
+    	cur.execute("CREATE TABLE Queries(Id INT, Query TEXT, Url TEXT)")
+    except:
+    	pass
+    i = 0
+    for word in words:
+    	try:
+    		output = searchScrape(word)
+    		cur.execute("INSERT INTO Queries VALUES(?,?,?)", (i, output[0], output[1]))
+    		conn.commit()
+    		i += 1
+    	except:
+    		print('Error!')
+    		pass
+    	sleep(2)
+
+# move to actual db
+os.rename("./queries-temp.db", "./queries.db")
