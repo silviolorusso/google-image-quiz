8d07c15d6da9dad4a692830bda5255f92f959196
lorussian <silviolorusso@gmail.com>
2014-07-13 12:56:37 +0200

    tabs

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ urls = (
 class index:
 	def GET(self):
 		term = searchDb()
-    # direct query to Google:
-    # term = search(randWord())
+		# direct query to Google:
+		# term = search(randWord())
 		return render.index(term[0], term[1])
 
 web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
